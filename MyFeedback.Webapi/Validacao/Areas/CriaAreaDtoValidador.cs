f26f0d4eb26e4dd47877c58ccd3718aa9f1c173f
adriandamiao <79238503+AdrianDamiao@users.noreply.github.com>
2022-05-18 13:00:00 -0300

    Adiciona correções para as DTOs de Area

--- MyFeedback.Webapi/Validacao/Areas/CriaAreaDtoValidador.cs
+++ MyFeedback.Webapi/Validacao/Areas/CriaAreaDtoValidador.cs
@@ -8,14 +8,14 @@ namespace MyFeedback.Webapi.Validacao.Areas
         public CriaAreaDtoValidador()
         {
             RuleFor(a => a.Nome)
-                .NotEmpty().WithMessage("O nome da empresa deve ser preenchido.")
+                .NotEmpty().WithMessage("O nome da empresa é obrigatório.")
                 .Length(3, 150).WithMessage("O nome da empresa deve ter entre 3 e 150 caractéres.");
             
             RuleFor(a => a.Descricao)
                 .MaximumLength(255).WithMessage("A descrição da empresa não deve ultrapassar 255 caractéres.");
 
             RuleFor(a => a.EmpresaId)
-                .NotEmpty().WithMessage("O Id da empresa deve ser informado e não pode ser 0");
+                .NotEmpty().WithMessage("O Id da empresa deve ser informado e não pode ser 0.");
         }
     }
 }
